c322717da32351734f71f161fd172fff97b854a5
Slava <sh-e-95@mail.ru>
2022-02-21 19:43:29 +0500

    add read from file

--- lab4/src/com/company/Main.java
+++ lab4/src/com/company/Main.java
@@ -2,6 +2,16 @@ package com.company;
 
 import com.map.*;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
@@ -14,17 +24,23 @@ public class Main {
         ###.f
      */
     public static void main(String[] args){
-        System.out.println("print maze");
-        String[][] arr = new String[5][];
-        for (int i = 0;i < 5; i++){
-            Scanner in = new Scanner(System.in);
-            String str = in.nextLine();
-            arr[i] = str.split("");
+
+        try {
+            String path = new File("").getAbsolutePath()+"\\assets\\example.txt";
+            String content = new String(Files.readAllBytes(Paths.get(path)));
+            String[] fields = content.split("\n");
+            String[][] arr = new String[fields.length][];
+            for(int i=0; i < fields.length; i++){
+                arr[i] = fields[i].split("");
+            }
+            map _map = new map();
+            arr = _map.findPath(arr);
+            for(String [] str: arr){
+                System.out.println(String.join("", str));
+            }
         }
-        map map = new map();
-        arr = map.findPath(arr);
-        for(String [] str: arr){
-            System.out.println(String.join("", str));
+        catch(Exception ex){
+            ex.printStackTrace();
         }
     }
 }
