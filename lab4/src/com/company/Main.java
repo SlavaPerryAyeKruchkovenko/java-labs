d76caa6ffa14f9f8249c92705ca41a263aa5cb3a
Slava <sh-e-95@mail.ru>
2022-02-21 22:04:14 +0500

    correct stream

--- lab4/src/com/company/Main.java
+++ lab4/src/com/company/Main.java
@@ -4,6 +4,7 @@ import com.map.*;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     /*
@@ -28,11 +30,9 @@ public class Main {
         try {
             String path = new File("").getAbsolutePath()+"\\assets\\example.txt";
             String content = new String(Files.readAllBytes(Paths.get(path)));
-            String[] fields = content.split("\n");
-            String[][] arr = new String[fields.length][];
-            for(int i=0; i < fields.length; i++){
-                arr[i] = fields[i].split("");
-            }
+            String[][] arr = Arrays.stream(content.split("\n"))
+                    .map(s -> s.split(""))
+                    .toArray(String[][]::new);
             map _map = new map();
             arr = _map.findPath(arr);
             for(String [] str: arr){
