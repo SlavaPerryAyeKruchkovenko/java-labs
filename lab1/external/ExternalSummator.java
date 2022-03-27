1040d16c509308158f2da08ab2ca37705745d119
Slava <sh-e-95@mail.ru>
2022-03-27 18:26:48 +0500

    deleta old labs

--- lab1/external/ExternalSummator.java
+++
@@ -1,7 +0,0 @@
-package external;
-
-public class ExternalSummator{
-    public static int sum(int num1,int num2){
-        return num1+num2;
-    }
-}
--- lab12/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] arg){
-        int val = Integer.MAX_VALUE*2;
-        Integer.decode("java");
-        System.out.println(Integer.compare(val,22));
-    }
-}
