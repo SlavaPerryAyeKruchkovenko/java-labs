2da797706f0e7795938d6916d9496b9db4cc6793
Slava <sh-e-95@mail.ru>
2022-03-27 18:25:57 +0500

    add coments

--- lab6/src/main/java/com/util/ExceptionUtil.java
+++ lab6/src/main/java/com/util/ExceptionUtil.java
@@ -11,20 +11,29 @@ public class ExceptionUtil {
             appendStackTrace(throwable, stackTrace);
         return ex;
     }
-    private static void appendStackTrace(Throwable t, StackTraceElement[] elements) {
-        List<StackTraceElement> traceElements = new ArrayList<>();
-        concat(traceElements, t.getStackTrace());
-        concat(traceElements, elements);
-        t.setStackTrace(traceElements.toArray(new StackTraceElement[0]));
-    }
 
+    /**
+     * Split exception by Hierarchy
+     * for Example if Exception1 was thrown earlier, then Exception2.
+     * Exception2 will be upper then Exception1
+     * @param ex caught Exception
+     * @return list of Exception by hierarchy order
+     */
     private static List<Throwable> splitByHierarchy(Throwable ex) {
         List<Throwable> exs = new ArrayList<>();
         while (ex != null) {
             exs.add(ex);
-            System.out.println(ex.getMessage());
             ex = ex.getCause();
         }
         return exs;
     }
+    private static void appendStackTrace(Throwable t, StackTraceElement[] elements) {
+        List<StackTraceElement> traceElements = new ArrayList<>();
+        if(t != null){
+            concat(traceElements, t.getStackTrace());
+            concat(traceElements, elements);
+            t.setStackTrace(traceElements.toArray(new StackTraceElement[0]));
+        }
+    }
+
 }
