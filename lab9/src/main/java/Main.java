3a43e86a6e7b3e0cd63275f188af9c44c9d5137e
Slava <sh-e-95@mail.ru>
2022-04-17 12:37:07 +0500

    create program

---
+++ lab9/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+
+public class Main {
+    public static void main(String[] args){
+        LinkedHashSet<Integer> set1 = new LinkedHashSet<Integer>();
+        for(Integer o:set1){
+            Function a;
+            Predicate b;
+        }
+    }
+}
