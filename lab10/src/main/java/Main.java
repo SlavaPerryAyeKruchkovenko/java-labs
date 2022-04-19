d9b4b0e1c562c6312f1ad0d8d2d479174d0b6521
Slava <sh-e-95@mail.ru>
2022-04-20 00:18:17 +0500

    create models

--- lab10/src/main/java/Main.java
+++ lab10/src/main/java/Main.java
@@ -1,8 +1,11 @@
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args){
 
     }
     public static Map<String, Set<Result>> add(Map<String, Set<Result>> index, Map<String, String> texts) {
-        // ...
+        return null;
     }
 }
