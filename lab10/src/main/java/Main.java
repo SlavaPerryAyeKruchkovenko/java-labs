a1082c461e380069788fc43d47ee1bcdf503998b
Slava <sh-e-95@mail.ru>
2022-04-19 23:56:08 +0500

    create project

---
+++ lab10/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+
+    }
+    public static Map<String, Set<Result>> add(Map<String, Set<Result>> index, Map<String, String> texts) {
+        // ...
+    }
+}
