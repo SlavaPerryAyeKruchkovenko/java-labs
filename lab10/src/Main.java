10855371171587b8c46ca9a0ce701d95e489b6f3
Slava <sh-e-95@mail.ru>
2022-04-20 11:33:33 +0500

    add index creator

--- lab10/src/Main.java
+++ lab10/src/Main.java
@@ -1,21 +1,33 @@
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("i am gay");
         Map<String,String> texts = getTexts(new File("assets"));
-        
+        System.out.println("input data");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Map<String,Set<Result>> index = getIndex(str);
+        System.out.println(index);
     }
     public static Map<String, Set<Result>> add(Map<String, Set<Result>> index, Map<String, String> texts) {
         return null;
     }
-    public static Map<String,String> getTexts(File directory){
+    private static Map<String,Set<Result>> getIndex(String str){
+        Map<String,Set<Result>> index = new HashMap<>();
+        str = str.toLowerCase(Locale.ROOT);
+        if(str.length() < 3)
+            throw new RuntimeException("Small input string");
+        else
+            for (int i = 3; i <= str.length(); i++)
+                index.put(str.substring(0,i),new HashSet<>());
+
+        return index;
+    }
+    private static Map<String,String> getTexts(File directory){
         Map<String, String> texts = new HashMap<>();
         if(directory.isDirectory()){
             for(File file1: Objects.requireNonNull(directory.listFiles())){
@@ -23,7 +35,6 @@ public class Main {
                     if(file1.isFile()){
                         String data = new String(Files.readAllBytes(Paths.get(file1.getAbsolutePath())));
                         texts.put(file1.getName(),data);
-                        System.out.println(data);
                     }
                 }
                 catch (Exception ex){
