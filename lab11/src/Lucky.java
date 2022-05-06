e8a161997c33581a63ad7743714d1d9aa99ef467
Slava <sh-e-95@mail.ru>
2022-05-07 00:30:35 +0500

    com

--- lab11/src/Lucky.java
+++ lab11/src/Lucky.java
@@ -4,16 +4,27 @@ public class Lucky {
     static int x = 0;
     static int count = 0;
     private static final Object lock = new Object();
+    private static final Object lock2 = new Object();
 
     static class LuckyThread extends Thread {
         @Override
         public void run() {
-            synchronized (lock){
-                while (x < 999999) {
-                    x++;
-                    if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
-                            % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
-                        System.out.println(x);
+            int xCopy = 0;
+            while (true) {
+                synchronized (lock){
+                        if(x < 999999) {
+                            x++;
+                            xCopy = x;
+                        }
+                        else{
+                            break;
+                        }
+                }
+                if ((xCopy % 10) + (xCopy / 10) % 10 + (xCopy / 100) % 10 == (xCopy / 1000)
+                        % 10 + (xCopy / 10000) % 10 + (xCopy / 100000) % 10) {
+                    //System.out.println(x);
+                    System.out.println(xCopy + " " + Thread.currentThread().getName());
+                    synchronized (lock2){
                         count++;
                     }
                 }
@@ -27,12 +38,14 @@ public class Lucky {
         Thread t1 = new LuckyThread();
         Thread t2 = new LuckyThread();
         Thread t3 = new LuckyThread();
+
         t1.start();
         t2.start();
         t3.start();
         t1.join();
         t2.join();
         t3.join();
+        //System.out.println((time - System.currentTimeMillis())/1000);
         System.out.println("Total: " + count);
     }
 }
